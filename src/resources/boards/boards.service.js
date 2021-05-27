15c79513897f74dc2a9ab22f7a74ecf2554fb0ef
daricat <darina.talk@gmail.com>
2021-05-27 14:00:36 +0300

    feat: add doc for boards-service

--- src/resources/boards/boards.service.js
+++ src/resources/boards/boards.service.js
@@ -1,14 +1,50 @@
 const boardDB = require('./boards.memory.repository');
 const Boards = require('./boards.model');
 
+/**
+ * @function getAllBoards
+ * @description -- calls method from class [BoardDB]{@link boardDB} to get all boards
+ * @return {Promise<Boards[]>}
+ */
 const getAllBoards = () => boardDB.getAllBoards();
 
+/**
+ * @function getBoardById
+ * @description -- calls method from class [BoardDB]{@link boardDB} to get board by id
+ * @param {string} id
+ * @return {Promise<Boards>}
+ */
 const getBoardById = (id) => boardDB.getBoardById(id);
 
+/**
+ * @function addBoard
+ * @description -- calls method from class [BoardDB]{@link boardDB} to add board
+ * @param {Boards} board
+ * @return {Promise<Boards>}
+ */
 const addBoard = (board) => boardDB.addBoard(new Boards(board));
 
+/**
+ * @function updateBoard
+ * @description -- calls method from class [BoardDB]{@link boardDB} to update board
+ * @param {string} id
+ * @param {Boards} fields
+ * @return {Promise<Boards>}
+ */
 const updateBoard = (id, fields) => boardDB.updateBoard(id, fields);
 
+/**
+ * @function deleteBoard
+ * @description -- calls method from class [BoardDB]{@link boardDB} for deleting board
+ * @param {string} id
+ * @return {Promise}
+ */
 const deleteBoard = (id) => boardDB.deleteBoard(id);
 
-module.exports = { getAllBoards, getBoardById, addBoard, updateBoard, deleteBoard };
+module.exports = {
+  getAllBoards,
+  getBoardById,
+  addBoard,
+  updateBoard,
+  deleteBoard,
+};
