847e8af1c748a72200704c27711e142664767080
daricat <darina.talk@gmail.com>
2021-05-27 17:43:17 +0300

    feat: add doc for task-service

--- src/resources/tasks/tasks.service.js
+++ src/resources/tasks/tasks.service.js
@@ -1,14 +1,45 @@
 const taskDB = require('./tasks.memory.repository');
 const Tasks = require('./tasks.model');
 
+/**
+ * @function getAllTasks
+ * @description -- calls method from class [TaskDB]{@link taskDB} to get all tasks
+ * @return {Promise<Tasks[]>}
+ */
 const getAllTasks = (boardId) => taskDB.getAllTasks(boardId);
 
+/**
+ * @function getTaskById
+ * @description -- calls method from class [TaskDB]{@link taskDB} to get tasks by id
+ * @param {string} boardId
+ * @param {string} taskId
+ * @return {Promise<Tasks>}
+ */
 const getTaskById = (boardId, taskId) => taskDB.getTaskById(boardId, taskId);
 
+/**
+ * @function addTask
+ * @description -- calls method from class [TaskDB]{@link taskDB} to add new task
+ * @param {Task} task
+ * @return {Promise<Tasks>}
+ */
 const addTask = (task) => taskDB.addTask(new Tasks(task));
 
+/**
+ * @function updateTask
+ * @description -- calls method from class [TaskDB]{@link taskDB} to update task
+ * @param {Task} task
+ * @return {Promise<Tasks>}
+ */
 const updateTask = (boardId, taskId, fields) => taskDB.updateTask(boardId, taskId, fields);
 
+/**
+ * @function deleteTask
+ * @description -- calls method from class [TaskDB]{@link taskDB} to delete task
+ * @param {string} boardId
+ * @param {string} taskId
+ * @return {Promise}
+ */
 const deleteTask = (boardId, taskId) => taskDB.deleteTask(boardId, taskId);
 
 module.exports = { getAllTasks, getTaskById, addTask, updateTask, deleteTask };
