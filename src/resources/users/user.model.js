b8d5bb83223e1195232307e5148df0a37a3abd55
daricat <darina.talk@gmail.com>
2021-05-27 18:17:23 +0300

    feat: add doc for user-model

--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,11 +1,28 @@
 const uuid = require('uuid').v4;
 
+/** Type definition for User.
+ * @typedef {Object} UserInfo
+ * @property {string} id
+ * @property {string} name
+ * @property {string} login
+ * @property {string} password
+ */
+
+/** Type definition for User.
+ * @typedef {Object} User
+ * @param {UserInfo} [userInfo={}] -- information about user
+ * @property {string} [id=uuid()]
+ * @property {string} [name='USER']
+ * @property {string} [login='user']
+ * @property {string} [password='P@55w0rd']
+ */
+
 class User {
   constructor({
     id = uuid(),
     name = 'USER',
     login = 'user',
-    password = 'P@55w0rd'
+    password = 'P@55w0rd',
   } = {}) {
     this.id = id;
     this.name = name;
@@ -13,6 +30,12 @@ class User {
     this.password = password;
   }
 
+  /**
+   * @method toResponse
+   * @static
+   * @description -- hide user password
+   * @param {User} user
+   */
   static toResponse(user) {
     const { id, name, login } = user;
     return { id, name, login };
