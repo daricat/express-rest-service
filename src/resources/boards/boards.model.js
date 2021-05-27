6697310ca7f5daddfecffed90a80954692cd1964
daricat <darina.talk@gmail.com>
2021-05-27 18:30:49 +0300

    refactor: fix doc in board-model

--- src/resources/boards/boards.model.js
+++ src/resources/boards/boards.model.js
@@ -1,31 +1,34 @@
 const uuid = require('uuid').v4;
 
 /** Type definition for Column.
- * @typedef {Object} Column
+ * @typedef {Object} ColumnInfo
  * @property {string} id
  * @property {string} title
  * @property {number} order
  */
 
+/** Type definition for Column.
+ * @typedef {Object} BoardInfo
+ * @property {string} id
+ * @property {string} title
+ * @property {number} order
+ */
 
 /** Type definition for Boad.
+ * @class
+ * @constructor
+ * @public
  * @typedef {Object} Boards
- * @param {Object} [boardInfo={}] -- information about board
+ * @param {Object} [BoardInfo={}] -- information about board
+ * @property {string} [id=uuid()]
+ * @property {string} [title='title']
+ * @property {ColumnInfo[]} [columns=[]]
  */
 
 class Boards {
   constructor({ id = uuid(), title = 'title', columns = [] } = {}) {
-    /**
-     * @property {string} id
-     */
     this.id = id;
-    /**
-     * @property {string} title
-     */
     this.title = title;
-    /**
-     * @property {Column[]} [columns=[]]
-     */
     this.columns = columns;
   }
 }
